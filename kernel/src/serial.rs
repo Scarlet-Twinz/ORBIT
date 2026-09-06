df3b12f5b2deed19afd199818b3aba57966e0a27
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-05 20:09:43 -0700

    Fix serial driver for uart_16550 0.8 API

--- kernel/src/serial.rs
+++ kernel/src/serial.rs
@@ -1,21 +1,29 @@
 use core::fmt;
 
 use spin::Mutex;
-use uart_16550::SerialPort;
+use uart_16550::{Config, Uart16550Tty};
 
-static SERIAL1: Mutex<SerialPort> = Mutex::new(unsafe { SerialPort::new(0x3F8) });
+static SERIAL1: Mutex<Option<Uart16550Tty>> = Mutex::new(None);
 
 pub fn init() {
-    SERIAL1.lock().init();
+    let mut serial = SERIAL1.lock();
+
+    if serial.is_none() {
+        let uart = unsafe {
+            Uart16550Tty::new_port(0x3F8, Config::default())
+                .expect("failed to create COM1 UART")
+        };
+        *serial = Some(uart);
+    }
 }
 
 pub fn _print(args: fmt::Arguments<'_>) {
     use fmt::Write;
 
-    SERIAL1
-        .lock()
-        .write_fmt(args)
-        .expect("serial output failed");
+    let mut serial = SERIAL1.lock();
+    if let Some(uart) = serial.as_mut() {
+        uart.write_fmt(args).expect("serial output failed");
+    }
 }
 
 #[macro_export]
