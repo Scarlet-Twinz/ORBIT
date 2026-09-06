c3f7d401e41a4af78bbbbffdf07140a110c7b99d
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-05 19:50:39 -0700

    feat(kernel): add COM1 serial console

---
+++ kernel/src/serial.rs
@@ -0,0 +1,33 @@
+use core::fmt;
+
+use spin::Mutex;
+use uart_16550::SerialPort;
+
+static SERIAL1: Mutex<SerialPort> = Mutex::new(unsafe { SerialPort::new(0x3F8) });
+
+pub fn init() {
+    SERIAL1.lock().init();
+}
+
+pub fn _print(args: fmt::Arguments<'_>) {
+    use fmt::Write;
+
+    SERIAL1.lock().write_fmt(args).expect("serial output failed");
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    () => {
+        $crate::serial::_print(format_args!("\n"))
+    };
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!(concat!($($arg)*, "\n")))
+    };
+}
